0af39a3f5225f4a0a6156272096a77c64a65eb08
Adam Asher <asheradamr@gmail.com>
2019-09-02 01:38:10 -0500

    weeklychallenge

--- weeklyChallenges01/ChallengesWithTest01.cs
+++ weeklyChallenges01/ChallengesWithTest01.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Numerics;
 
 namespace ChallengesWithTests01
 {
@@ -8,36 +9,64 @@ namespace ChallengesWithTests01
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
+            if(num1 == num2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             throw new NotImplementedException();
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
+            double sub = minuend - subtrahend;
+            return sub;
             throw new NotImplementedException();
         }
 
         public int Add(int number1, int number2)
         {
+            int sum = number1 + number2;
+            return sum;
             throw new NotImplementedException();
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+           int tinyboi = Math.Min(number1,number2);
+           return tinyboi;
+           throw new NotImplementedException();
         }
 
         public long Multiply(long factor1, long factor2)
         {
+            long twoply = factor1 * factor2;
+            return twoply;
             throw new NotImplementedException();
         }
 
-        public string GetGreeting(string nameOfPerson)
+        public string GetGreeting(string personName) //Changed nameOfPerson to personName
         {
+            string greet = "Hello!";
+            string greetings = "Hello, " + personName + "!";
+            if (string.IsNullOrEmpty(personName))
+            {
+                return greet;
+            }
+            else
+            {
+                return greetings;
+            }
             throw new NotImplementedException();
         }
 
         public string GetHey()
         {
+            string hey = "HEY!";
+            return hey;
             throw new NotImplementedException();
         }
     }
